21bea2083c5a152d1f6dd02d5e7db6fe5ee8b66e
23RoMax <max.nebl@googlemail.com>
2020-05-04 22:05:18 +0200

    chore(logging): removed unnecessary function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var wifi = require('Wifi');
 var http = require('http');
 
 function connectWifi() {
-  logger('Connecting to WiFi');
+  console.log('Connecting to WiFi');
   d.drawString("Connecting to WiFi",2,15);
   wifi.connect(ssid, {password: password}, function() {
       console.log('Connected to Wifi.  IP address is:', wifi.getIP().ip);
@@ -20,19 +20,17 @@ function connectWifi() {
   });
 }
 
-function logger( arg1 ) {
-  console.log( arg1 );
-}
-
 function getBitcoinEur() {
-  logger('Fetching BTC');
+  console.log('Fetching BTC');
+  // using http as https is only possible with recompiling the firmware 
+  // See: https://github.com/espruino/Espruino/issues/1777#issuecomment-618232966
   let url = 'http://api.coindesk.com/v1/bpi/currentprice/EUR.json';
 
   http.get(url, function(res) {
     res.on('data', function(data) {
       let result = data.replace(/(\r\n\t|\n|\r\t)/gm,"");
       result = JSON.parse(result);
-      logger(result);
+      console.log(result);
       d.clear();
       let string = result.bpi.EUR.rate + ' EUR';
       printString('1 BTC', 50, 20, true);
@@ -75,6 +73,6 @@ function clearDisplay() {
 I2C1.setup({scl:D22,sda:D21});
 var d = require("SSD1306").connect(I2C1, show);
 // Getting state of esp on start
-logger(ESP32.getState());
+console.log(ESP32.getState());
 // Connect to Wifi
 connectWifi();
